d70c935696f6cfeeee613d23fab50d0c86078b74
Julius <74532956+juliusvara@users.noreply.github.com>
2020-12-22 10:07:06 +0200

    commit

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
+import { Clock } from './components/clock/Clock.js';
+
 import { progressBarData } from './data/progressBarData.js';
 import { renderProgressBar } from "./components/progress-bar/renderProgressBar.js";
 
 import { socialsData } from './data/socialsData.js';
 import { renderSocials } from './components/socials/renderSocials.js';
 
+const clock = new Clock('.hero .clock', {
+    month: 1,
+    day: 1,
+    hour: 0,
+    minutes: 0,
+    seconds: 0
+});
+clock.init();
+
 renderProgressBar('.left', progressBarData);
 
 renderSocials('footer > .row-long', socialsData);
+
